adcea3bbd5aee702203e8e268ec78acff530960c
Shamari Blackwoood <shamariblackw@gmail.com>
2025-03-23 23:41:10 -0500

    updated js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,24 @@
 window.onload = function() {
-    // load navigation bar/menu
-    fetch("../templates/nav.html")
-    .then(response => response.text())
-    .then(data => {document.getElementById("nav").innerHTML = data});
 
-    // load footer
-    fetch("../templates/footer.html")
-    .then(response => response.text())
-    .then(data => {document.getElementById("footer").innerHTML = data});
+    if (window.location.href.includes("theeshamari.github.io")) {
+        // load navigation bar/menu
+        fetch("https://theeshamari.github.io/thee-Ultra/templates/nav.html")
+        .then(response => response.text())
+        .then(data => {document.getElementById("nav").innerHTML = data});
+
+        // load footer
+        fetch("https://theeshamari.github.io/thee-Ultra/templates/footer.html")
+        .then(response => response.text())
+        .then(data => {document.getElementById("footer").innerHTML = data});
+    } else {
+        // load navigation bar/menu
+        fetch("../templates/nav.html")
+        .then(response => response.text())
+        .then(data => {document.getElementById("nav").innerHTML = data});
+    
+        // load footer
+        fetch("../templates/footer.html")
+        .then(response => response.text())
+        .then(data => {document.getElementById("footer").innerHTML = data});
+    }
 }
